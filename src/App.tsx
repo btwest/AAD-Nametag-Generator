a79b38ca1eb59dad41c1511413db6988599a87c9
Brendan West <brendantwest@gmail.com>
2026-06-11 13:09:10 -0400

    Reverted append logic

--- src/App.tsx
+++ src/App.tsx
@@ -210,24 +210,7 @@ function App() {
           },
         );
 
-        if (append) {
-          let merged = [...tags];
-          for (const newTag of newTags) {
-            const isRealId = !newTag.id.startsWith("tag-");
-            const existingIndex = isRealId
-              ? merged.findIndex((t) => t.id === newTag.id)
-              : -1;
-
-            if (existingIndex !== -1) {
-              merged[existingIndex] = newTag;
-            } else {
-              merged.push(newTag);
-            }
-          }
-          setTags(merged);
-        } else {
-          setTags(newTags);
-        }
+        setTags(append ? [...tags, ...newTags] : newTags);
       },
     });
 
